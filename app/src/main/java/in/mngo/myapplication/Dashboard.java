a7f75ab080825e69017cd6fbee646673a9c19ad1
adityasuman2025 <aditya.me16@iitp.ac.in>
2019-06-15 22:05:54 +0530

    csv to JSON done

--- app/src/main/java/in/mngo/myapplication/Dashboard.java
+++ app/src/main/java/in/mngo/myapplication/Dashboard.java
@@ -6,12 +6,17 @@ import android.widget.TextView;
 
 import com.opencsv.CSVReader;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Dashboard extends AppCompatActivity
 {
@@ -25,25 +30,52 @@ public class Dashboard extends AppCompatActivity
 
         text = findViewById(R.id.text);
 
-        String result = "";
+
+    //to open csv file and store it in JSON
+        ArrayList<String> title = new ArrayList<String>();
+        JSONArray jsonArray = new JSONArray();
+
         try
         {
             InputStream is = this.getResources().openRawResource(R.raw.saran);
-
             CSVReader reader = new CSVReader(new InputStreamReader(is, "UTF8"));
+
+        //to get the title of the array
             String[] nextLine;
             while ((nextLine = reader.readNext()) != null)
             {
-            // nextLine[] is an array of values from the line
-                result += (nextLine[0] + nextLine[1] + "etc...");
+                int len = nextLine.length;
+                for(int i= 0; i< len; i++)
+                {
+                    title.add(nextLine[i]);
+                }
+
+                break;
+            }
+
+        //to get elements of the csv file as JSON
+            while ((nextLine = reader.readNext()) != null)
+            {
+                int len = nextLine.length;
+
+                JSONObject item = new JSONObject();
+
+                for(int i= 0; i< len; i++)
+                {
+                    item.put(title.get(i), nextLine[i]);
+                }
+
+                jsonArray.put(item);
             }
         }
         catch (IOException e)
         {
-
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
 
-        text.setText(result);
+        text.setText(jsonArray.toString());
 
     }
 }
